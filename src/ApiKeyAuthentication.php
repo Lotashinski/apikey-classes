dd03c815c66c22382f12e2a6c223584339596e3a
Alexander Lotashinsky <allotashinsky@yandex.by>
2021-12-13 14:27:27 +0300

    add strict verification

--- src/ApiKeyAuthentication.php
+++ src/ApiKeyAuthentication.php
@@ -11,41 +11,42 @@ use Symfony\Component\Security\Http\Authenticator\AbstractAuthenticator;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\CustomCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
-use Symfony\Component\Security\Http\Authenticator\Passport\SelfValidatingPassport;
 
 
 class ApiKeyAuthentication
     extends AbstractAuthenticator
 {
 
-    public const HEADER = 'X-AUTH-KEY';
-
     private LoggerInterface $logger;
-
+    private bool $strictVerification;
+    private string $header;
 
     public function __construct(
-        LoggerInterface $logger
+        LoggerInterface $logger,
+        string          $header = 'X-AUTH-KEY',
+        bool            $strictVerification = false
     )
     {
         $this->logger = $logger;
+        $this->header = $header;
+        $this->strictVerification = $strictVerification;
     }
 
 
     public function supports(Request $request): ?bool
     {
-        $isSupport = $request->headers->has(self::HEADER);
+        $isSupport = $this->strictVerification || $request->headers->has($this->header);
 
         $this->logger->debug('Check request Authenticator support', [
-            'isSupport' => $isSupport,
-            'headers' => $request->headers->all(),
-        ]);;
+            'headers-key' => $request->headers->get($this->header),
+        ]);
 
         return $isSupport;
     }
 
     public function authenticate(Request $request): Passport
     {
-        $userBadge = new UserBadge($request->headers->get(self::HEADER));
+        $userBadge = new UserBadge($request->headers->get($this->header));
 
         $credentialChecker = new CustomCredentials(
             function (?string $ip, ApiKeyUser $user) {
